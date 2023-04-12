ee43624e729138e504f931722795283c6b71e786
Ethan Black <ethanrossblack@gmail.com>
2023-04-12 17:45:55 -0600

    Add array methods and explanations

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ strings = ["violin", "viola", "cello", "bass"]
 integers = [1, 2, 3, 4]
 floats = [1.1, 1.2, 1.3, 1.4]
 booleans = [true, true, false, false]
+
+# This will remove and return the last item in the "floats" array => 1.4
+floats.pop
+
+# This will add "banjo" to the end of the "strings" array 
+strings.push("banjo")
+
+# This will remove the first two itens from the "integers" array and return them in another array => [1,2]
+integers.shift(2)
+
+# This will add a "false" value to the beginning of the "booleans" array
+booleans.unshift(false)
